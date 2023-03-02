754886e5faa7caeee2bbf3886c11b57b5d2e6283
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-03-02 20:41:27 +0500

    Add dz 14

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int DoubleCheck(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] myArray = GenerateArray(5, 100, 1000);
+PrintArray(myArray);
+System.Console.WriteLine(DoubleCheck(myArray));
